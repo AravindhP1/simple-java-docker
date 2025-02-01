f13e21e307635dcd943cb6dbf90f2e0449bef631
AravindhP1 <aravindhaws1@gmail.com>
2025-02-01 22:46:40 +0530

    variables.tf

---
+++ terraform-eks/variables.tf
@@ -0,0 +1,43 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  default     = "my-eks-cluster"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidrs" {
+  description = "Subnets CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for worker nodes"
+  default     = "t3.medium"
+}
+
+variable "desired_capacity" {
+  default = 2
+}
+
+variable "max_capacity" {
+  default = 3
+}
+
+variable "min_capacity" {
+  default = 1
+}
+
+variable "availability_zones" {
+  description = "List of availability zones for subnets"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]  # Ensure these exist in your AWS region
+}
