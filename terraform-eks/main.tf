5665332e1bd7265c2cc125fa9df07bd32ac301fc
AravindhP1 <aravindhaws1@gmail.com>
2025-02-06 15:28:19 +0530

    main.tf

--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -205,12 +205,6 @@ resource "aws_eks_node_group" "eks_nodes" {
     min_size     = var.min_capacity
   }
 
-  # Allow SSH access to worker nodes
-  remote_access {
-    ec2_ssh_key               = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.eks_node_sg.id]
-  }
-
   tags = {
     Name = "eks-worker-nodes"
   }
